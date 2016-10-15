d6aae9960bace447151dcb3da479790a4ecf4611
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2016-10-15 15:57:52 +0200

    update the openvpn command to stick with the new required configuration

--- lille1-vpn.sh
+++ lille1-vpn.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 openvpn_config="/usr/local/etc/lille1-vpn.conf"
-openvpn --config "$openvpn_config"
+openvpn --config "$openvpn_config" --script-security 2 --up /etc/openvpn/update-resolv-conf --down /etc/openvpn/update-resolv-conf
